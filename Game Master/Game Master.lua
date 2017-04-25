60513aebc8b9427fd33ffedbd6c20cdf45f13587
IlseElysium <ilse.elysium@gmail.com>
2017-04-26 00:28:52 +0100

    Update so it doesn't shows the GM options to yourself

--- Game Master/Game Master.lua
+++ Game Master/Game Master.lua
@@ -168,3 +168,166 @@ function UnitPopup_OnClick()
   end
   PlaySound("UChatScrollButton");
 end
+
+function UnitPopup_ShowMenu(dropdownMenu, which, unit, name, userData)
+  -- Init variables
+  dropdownMenu.which = which;
+  dropdownMenu.unit = unit;
+  if ( unit and not name ) then
+    name, server = UnitName(unit, true);
+  end
+  dropdownMenu.name = name;
+  dropdownMenu.userData = userData;
+  dropdownMenu.server = server;
+ 
+  -- Determine which buttons should be shown or hidden
+  UnitPopup_HideButtons();
+ 
+  -- If only one menu item (the cancel button) then don't show the menu
+  local count = 0;
+  for index, value in UnitPopupMenus[which] do
+    if( UnitPopupShown[index] == 1 and value ~= "CANCEL" ) then
+      count = count + 1;
+    end
+  end
+  if ( count < 1 ) then
+    return;
+  end
+ 
+  -- Determine which loot method and which loot threshold are selected and set the corresponding buttons to the same text
+  dropdownMenu.selectedLootMethod = UnitLootMethod[GetLootMethod()].text;
+  UnitPopupButtons["LOOT_METHOD"].text = dropdownMenu.selectedLootMethod;
+  UnitPopupButtons["LOOT_METHOD"].tooltipText = UnitLootMethod[GetLootMethod()].tooltipText;
+  dropdownMenu.selectedLootThreshold = getglobal("ITEM_QUALITY"..GetLootThreshold().."_DESC");
+  UnitPopupButtons["LOOT_THRESHOLD"].text = dropdownMenu.selectedLootThreshold;
+  -- This allows player to view loot settings if he's not the leader
+  if ( ((GetNumPartyMembers() > 0) or (GetNumRaidMembers() > 0)) and IsPartyLeader() ) then
+    -- If this is true then player is the party leader
+    UnitPopupButtons["LOOT_METHOD"].nested = 1;
+    UnitPopupButtons["LOOT_THRESHOLD"].nested = 1;
+  else
+    UnitPopupButtons["LOOT_METHOD"].nested = nil;
+    UnitPopupButtons["LOOT_THRESHOLD"].nested = nil;
+  end
+ 
+  -- If level 2 dropdown
+  local info;
+  local color;
+  local icon;
+  if ( UIDROPDOWNMENU_MENU_LEVEL == 2 ) then
+    dropdownMenu.which = UIDROPDOWNMENU_MENU_VALUE;
+    -- Set which menu is being opened
+    OPEN_DROPDOWNMENUS[UIDROPDOWNMENU_MENU_LEVEL] = {which = dropdownMenu.which, unit = dropdownMenu.unit};
+    for index, value in UnitPopupMenus[UIDROPDOWNMENU_MENU_VALUE] do
+      info = {};
+      info.text = UnitPopupButtons[value].text;
+      info.owner = UIDROPDOWNMENU_MENU_VALUE;
+      -- Set the text color
+      color = UnitPopupButtons[value].color;
+      if ( color ) then
+        info.textR = color.r;
+        info.textG = color.g;
+        info.textB = color.b;
+      end
+      -- Icons
+      info.icon = UnitPopupButtons[value].icon;
+      info.tCoordLeft = UnitPopupButtons[value].tCoordLeft;
+      info.tCoordRight = UnitPopupButtons[value].tCoordRight;
+      info.tCoordTop = UnitPopupButtons[value].tCoordTop;
+      info.tCoordBottom = UnitPopupButtons[value].tCoordBottom;
+      -- Checked conditions
+      if ( info.text == dropdownMenu.selectedLootMethod  ) then
+        info.checked = 1;
+      elseif ( info.text == dropdownMenu.selectedLootThreshold ) then
+        info.checked = 1;
+      elseif ( strsub(value, 1, 12) == "RAID_TARGET_" ) then
+        local raidTargetIndex = GetRaidTargetIndex(unit);
+        if ( raidTargetIndex == index ) then
+          info.checked = 1;
+        end
+      end
+     
+      info.value = value;
+      info.func = UnitPopup_OnClick;
+      -- Setup newbie tooltips
+      info.tooltipTitle = UnitPopupButtons[value].text;
+      info.tooltipText = getglobal("NEWBIE_TOOLTIP_UNIT_"..value);
+      UIDropDownMenu_AddButton(info, UIDROPDOWNMENU_MENU_LEVEL);
+    end
+    return;    
+  end
+ 
+  -- Add dropdown title
+  if ( unit or name ) then
+    info = {};
+    if ( name ) then
+      info.text = name;
+    else
+      info.text = TEXT(UNKNOWN);
+    end
+    info.isTitle = 1;
+    info.notCheckable = 1;
+    UIDropDownMenu_AddButton(info);
+  end
+ 
+  -- Set which menu is being opened
+  OPEN_DROPDOWNMENUS[UIDROPDOWNMENU_MENU_LEVEL] = {which = dropdownMenu.which, unit = dropdownMenu.unit};
+  -- Show the buttons which are used by this menu
+  local tooltipText;
+  for index, value in UnitPopupMenus[which] do
+    if ( index > 5 and index < 10 and DropDownList1Button1.value == UnitName("player") ) then
+      UnitPopupShown[index] = 0;
+    end
+    if ( UnitPopupShown[index] == 1 ) then
+      info = {};
+      info.text = UnitPopupButtons[value].text;
+      info.value = value;
+      info.owner = which;
+      info.func = UnitPopup_OnClick;
+      if ( not UnitPopupButtons[value].checkable ) then
+        info.notCheckable = 1;
+      end
+      -- Text color
+      if ( value == "LOOT_THRESHOLD" ) then
+        -- Set the text color
+        color = ITEM_QUALITY_COLORS[GetLootThreshold()];
+        info.textR = color.r;
+        info.textG = color.g;
+        info.textB = color.b;
+      else
+        color = UnitPopupButtons[value].color;
+        if ( color ) then
+          info.textR = color.r;
+          info.textG = color.g;
+          info.textB = color.b;
+        end
+      end
+      -- Icons
+      info.icon = UnitPopupButtons[value].icon;
+      info.tCoordLeft = UnitPopupButtons[value].tCoordLeft;
+      info.tCoordRight = UnitPopupButtons[value].tCoordRight;
+      info.tCoordTop = UnitPopupButtons[value].tCoordTop;
+      info.tCoordBottom = UnitPopupButtons[value].tCoordBottom;
+      -- Checked conditions
+      if ( strsub(value, 1, 12) == "RAID_TARGET_" ) then
+        local raidTargetIndex = GetRaidTargetIndex("target");
+        if ( raidTargetIndex == index ) then
+          info.checked = 1;
+        end
+      end
+      if ( UnitPopupButtons[value].nested ) then
+        info.hasArrow = 1;
+      end
+     
+      -- Setup newbie tooltips
+      info.tooltipTitle = UnitPopupButtons[value].text;
+      tooltipText = getglobal("NEWBIE_TOOLTIP_UNIT_"..value);
+      if ( not tooltipText ) then
+        tooltipText = UnitPopupButtons[value].tooltipText;
+      end
+      info.tooltipText = tooltipText;
+      UIDropDownMenu_AddButton(info);
+    end
+  end
+  PlaySound("igMainMenuOpen");
+end
